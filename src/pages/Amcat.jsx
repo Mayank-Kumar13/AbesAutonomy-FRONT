d58dc458afdadb9f584a793cc89f4c16ba2326ef
Mayank <mayankkotuli099@gmail.com>
2026-09-05 17:50:59 +0530

    Make AMCAT page image responsive

--- src/pages/Amcat.jsx
+++ src/pages/Amcat.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Amcat = () => {
   return (
-    <div>
-      <img src="/AMCAT_SOON.jpeg" alt="Amcat" />
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', backgroundColor: '#090c10', padding: '20px 0' }}>
+      <img src="/AMCAT_SOON.jpeg" alt="Amcat" style={{ maxWidth: '100%', height: 'auto' }} />
     </div>
   )
 }
