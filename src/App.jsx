5a8d5085e28029f6702d03af474f0e66222d7a03
Mayank <mayankkotuli099@gmail.com>
2026-09-26 01:41:52 +0530

    fix: enable privacy policy page route

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import { useLocation } from "react-router-dom";
 import LoginPage from './pages/loginPage/LoginPage';
 import Profile from './pages/profile/Profile';
 import About from './pages/InfoPage/About';
-// import PrivacyPolicy from './pages/InfoPage/PrivacyPolicy';
+import PrivacyPolicy from './pages/InfoPage/PrivacyPolicy';
 import Terms from './pages/InfoPage/Terms';
 import Contact from './pages/InfoPage/Contact';
 import Contribute from './pages/InfoPage/Contribute';
@@ -59,6 +59,7 @@ const App = () => {
           <Route path="/auth/google/callback" element={<ProviderCallback provider="google" />} />
           <Route path="/auth/github/callback" element={<ProviderCallback provider="github" />} />
           <Route path="/about" element={<About/>}/>
+          <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/contribute" element={<Contribute />} />
